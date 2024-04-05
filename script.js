9ebf60bfa8a061ecc616d4bd03d523d39cf290bd
tmbc12 <153978166+tmbc12@users.noreply.github.com>
2024-04-05 11:06:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ loader.load(
     texture.minFilter = THREE.LinearFilter;
     
     loader.load(
-      'https://i.ibb.co/K9DnNzt/Frame-427322079.png',  
+      'https://i.ibb.co/3Wj2D3R/Frame-427322079-1.png',  
           (tex) => {
         _500 = tex;
         
